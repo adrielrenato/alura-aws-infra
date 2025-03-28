8460f0e1ec8f64bdcd0b00f2484bc2776a5d5932
Adriel Renato <adriel.renato26@gmail.com>
2025-03-28 16:12:08 -0300

    definindo o health check para que o load balancer não de erro ao procurar uma rota que funcione

--- src/main/java/com/myorg/AluraServiceStack.java
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -4,11 +4,14 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.vpclattice.CfnTargetGroup;
 import software.constructs.Construct;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.Duration;
+
+import java.util.List;
 
 public class AluraServiceStack extends Stack {
     public AluraServiceStack(final Construct scope, final String id, final Cluster cluster) {
@@ -18,22 +21,24 @@ public class AluraServiceStack extends Stack {
     public AluraServiceStack(final Construct scope, final String id, final StackProps props, final Cluster cluster) {
         super(scope, id, props);
 
-        // Create a load-balanced Fargate service and make it public
-        ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
+//         Criando um serviço Fargate balanceado com o Application Load Balancer e tornando-o público
+        var app = ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
                 .serviceName("alura-service-ola")
-                .cluster(cluster)           // Required
-                .cpu(512)                   // Default is 256
-                .desiredCount(1)            // Default is 1
-                .listenerPort(8080)
-                .assignPublicIp(true)
+                .cluster(cluster)           // Cluster ECS para onde o serviço será implantado
+                .cpu(512)                   // Configuração de CPU (padrão é 256)
+                .desiredCount(1)            // Número desejado de instâncias (padrão é 1)
+                .listenerPort(8080)         // Porta para o listener do ALB
+                .assignPublicIp(true)       // Atribui um IP público ao serviço
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
-                                .image(ContainerImage.fromRegistry("jacquelineoliveira/ola:1.0"))
-                                .containerPort(8080)
-                                .containerName("app_ola")
+                                .image(ContainerImage.fromRegistry("jacquelineoliveira/ola:1.0"))  // Imagem do Docker
+                                .containerPort(8080)  // A porta do container
+                                .containerName("app_ola")  // Nome do container
                                 .build())
-                .memoryLimitMiB(1024)
-                .publicLoadBalancer(true)   // Default is false
+                .memoryLimitMiB(2048)  // Limite de memória para o serviço
+                .publicLoadBalancer(true)   // Habilita o Load Balancer público
                 .build();
+
+        app.getTargetGroup().configureHealthCheck(HealthCheck.builder().path("/ola").build());
     }
 }
