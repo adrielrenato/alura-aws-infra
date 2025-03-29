95521b18a6e3c9e583d6272a680eadffb0f0edf7
Adriel Renato <adriel.renato26@gmail.com>
2025-03-29 01:15:43 -0300

    Criando um autoscaling das instancias caso atinja 70 da cpu ou 70 da memoria durante 3 minutos diretos

--- src/main/java/com/myorg/AluraServiceStack.java
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -1,13 +1,14 @@
 package com.myorg;
 
-import software.amazon.awscdk.Fn;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 import java.util.HashMap;
@@ -27,8 +28,9 @@ public class AluraServiceStack extends Stack {
         autenticacao.put("SPRING_DATASOURCE_USERNAME", "admin");
         autenticacao.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-senha"));
 
-//         Criando um serviço Fargate balanceado com o Application Load Balancer e tornando-o público
-        ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
+        IRepository repository = Repository.fromRepositoryName(this, "repositorio", "img-pedidos-ms");
+
+        var aluraService = ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
                 .serviceName("alura-service-pedidos-ms")
                 .cluster(cluster)           // Cluster ECS para onde o serviço será implantado
                 .cpu(512)                   // Configuração de CPU (padrão é 256)
@@ -37,15 +39,39 @@ public class AluraServiceStack extends Stack {
                 .assignPublicIp(true)       // Atribui um IP público ao serviço
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
-                                .image(ContainerImage.fromRegistry("jacquelineoliveira/pedidos-ms"))  // Imagem do Docker
+                                .image(ContainerImage.fromEcrRepository(repository))  // Imagem do Docker
                                 .containerPort(8080)  // A porta do container
                                 .containerName("app_pedidos_ms")  // Nome do container
                                 .environment(autenticacao)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "PedidosMsLogGroup")
+                                                .logGroupName("PedidosMsLog")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("PedidosMS")
+                                        .build()))
                                 .build())
-                .memoryLimitMiB(2048)  // Limite de memória para o serviço
+                .memoryLimitMiB(1024)  // Limite de memória para o serviço
                 .publicLoadBalancer(true)   // Habilita o Load Balancer público
-                .build()
-                .getTargetGroup()
-                .configureHealthCheck(HealthCheck.builder().path("/pedidos").build());
+                .build();
+
+        aluraService.getTargetGroup().configureHealthCheck(HealthCheck.builder().path("/pedidos").build());
+
+        ScalableTaskCount scalableTarget = aluraService.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(3)
+                .build());
+
+        scalableTarget.scaleOnCpuUtilization("CpuScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
+
+        scalableTarget.scaleOnMemoryUtilization("MemoryScaling", MemoryUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
     }
 }
